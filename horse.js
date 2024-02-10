995dce1f686839f31aff64e257d63b29d4b34969
thejoker2525 <34322027+thejoker2525@users.noreply.github.com>
2024-02-10 21:04:32 +0100

    Add files via upload

---
+++ horse.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    const introduction = document.getElementById('introduction');
+  
+    header.style.backgroundColor = '#4CAF50';
+    introduction.style.backgroundColor = '#F5F5F5';
+  });
